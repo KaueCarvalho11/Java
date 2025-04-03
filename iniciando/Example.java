702cf832c8c7523938f6f6e61d02c312987d9696
Kauê Carvalho <147278152+KaueCarvalho11@users.noreply.github.com>
2025-04-03 17:38:04 -0300

    Conceitos iniciais

---
+++ iniciando/Example.java
@@ -0,0 +1,36 @@
+package iniciando;
+
+/*
+Nome da classe principal deve coincidir com o nome do arquivo que contém o programa
+Importante para que nome do arquivo fonte coincida com o nome do arquivo.class
+
+*/
+class Example {
+
+    /*Todo código deve residir dentro de classes */
+    public static void main(String[] args) {
+        System.out.println("Hello, World"); 
+    }
+}
+
+/*
+Compilador: Javac
+
+Comando para compilar: javac package/nome-arquivo.java
+
+Compilação irá gerar versão em bytecode do programa: nome-arquivo.class
+ */
+
+/*
+Interpretador: Java
+
+comando para executar: java package.classe
+
+Dessa forma, especificando o nome da classe que deseja que o interpretador execute
+*/
+
+class Segundo {
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo"); 
+    }
+}
