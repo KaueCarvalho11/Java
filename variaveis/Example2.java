762f7c69446e87050626cb431fbc7c5659bb6710
Kauê Carvalho <147278152+KaueCarvalho11@users.noreply.github.com>
2025-04-04 13:46:58 -0300

    Trabalhando com variáveis do tipo int

---
+++ variaveis/Example2.java
@@ -0,0 +1,23 @@
+package variaveis;
+
+class Example2 {
+    public static void main(String[] args) {
+        int var1;
+        int var2;
+
+        var1 = 1024;
+        var2 = var1 / 2;
+
+        System.out.println("var1 = " + var1);
+        System.out.print("var2 = var1 / 2 = ");
+        System.out.print(var2);
+    }
+}
+
+class InnerExample2 {
+    public static void main(String[] args) {
+        int var3 = 10;
+        int var4 = 20;
+        System.out.println("var3 e var4: " + var3 + " e " + var4 );
+    }
+}
