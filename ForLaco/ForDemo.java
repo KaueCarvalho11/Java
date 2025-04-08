c9725718810e937f1f428cf5c566c4980821bf1d
Kauê Carvalho <147278152+KaueCarvalho11@users.noreply.github.com>
2025-04-08 09:32:32 -0300

    Trabalhando com o laço "for"

---
+++ ForLaco/ForDemo.java
@@ -0,0 +1,11 @@
+package ForLaco;
+
+class ForDemo {
+    public static void main(String[] args) {
+        
+        for(int count = 0; count <= 100; count++){
+            System.out.println("Contador = " + count);
+        }
+        System.out.println("Fim");
+    }
+}
