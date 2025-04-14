4c3793ce1690d7cd10e8e251d79e9761a495c719
Kauê Carvalho <147278152+KaueCarvalho11@users.noreply.github.com>
2025-04-14 15:07:37 -0300

    Trabalhando com caractere usando aritmética e sequência Ascii

---
+++ caractere/Ascii.java
@@ -0,0 +1,29 @@
+package caractere;
+
+class Ascii {
+    public static void main(String[]args){
+        char caractere = 'x';
+
+        System.out.println("caractere = " + caractere);
+
+        caractere++; 
+        System.out.println("caractere = " + caractere);
+
+        caractere = 25;
+
+        System.out.println("caractere = " + caractere);
+
+        caractere = 37;
+
+        System.out.println("Caractere = " + caractere);
+
+        caractere = 90;
+
+        System.out.println("Caractere = " + caractere);
+
+        caractere = 126;
+
+        System.out.println("Caractere = " + caractere);
+    }
+    
+}
